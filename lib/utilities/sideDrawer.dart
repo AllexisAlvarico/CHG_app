c9a991c173cd57e71ac6e4dd669b798e60b6a143
MichalKrzyzanowski <c00240696@itcarlow.ie>
2021-07-20 12:53:26 +0100

    changed theme colors of the side drawer

--- lib/utilities/sideDrawer.dart
+++ lib/utilities/sideDrawer.dart
@@ -1,3 +1,4 @@
+import 'package:chg/globals.dart';
 import 'package:flutter/material.dart';
 
 class SideDrawer extends StatelessWidget {
@@ -8,42 +9,68 @@ class SideDrawer extends StatelessWidget {
     return Container(
       child: SafeArea(
         child: Drawer(
-          child: ListView(
-            children: [
-              // This is a flutter premade widget
-              UserAccountsDrawerHeader(
-                accountEmail: Text(''),
-                accountName: Text(''),
-                otherAccountsPictures: [Icon(Icons.home)],
-                currentAccountPicture: CircleAvatar(
-                  backgroundImage: AssetImage('assets/images/Home/Home.png'),
+          child: Container(
+            color: backgroundColor,
+            child: ListView(
+              children: [
+                // This is a flutter premade widget
+                UserAccountsDrawerHeader(
+                  accountEmail: Text(''),
+                  accountName: Text(''),
+                  otherAccountsPictures: [
+                    Icon(
+                      Icons.home,
+                      color: textColor,
+                    )
+                  ],
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: AssetImage('assets/images/Home/Home.png'),
+                  ),
+                  decoration: BoxDecoration(
+                    color: appbarColor,
+                  ),
                 ),
-              ),
-              ListTile(
-                title: Text('Settings'),
-                leading: Icon(Icons.settings),
-                onTap: () => Navigator.pushReplacementNamed(
-                  context,
-                  'Settings',
+                ListTile(
+                  title: Text(
+                    'Settings',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  leading: Icon(Icons.settings, color: textColor),
+                  onTap: () => Navigator.pushReplacementNamed(
+                    context,
+                    'Settings',
+                  ),
                 ),
-              ),
-              ListTile(
-                title: Text('Events'),
-                leading: Icon(Icons.event),
-                onTap: () => Navigator.pushNamed(
-                  context,
-                  'Events',
+                ListTile(
+                  title: Text(
+                    'Events',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  leading: Icon(Icons.event, color: textColor),
+                  onTap: () => Navigator.pushNamed(
+                    context,
+                    'Events',
+                  ),
                 ),
-              ),
-              ListTile(
-                title: Text('Headquarters'),
-                leading: Icon(Icons.hotel),
-                onTap: () => Navigator.pushNamed(
-                  context,
-                  'Headquarters',
-                ),
-              )
-            ],
+                ListTile(
+                  title: Text(
+                    'Headquarters',
+                    style: TextStyle(
+                      color: textColor,
+                    ),
+                  ),
+                  leading: Icon(Icons.hotel, color: textColor),
+                  onTap: () => Navigator.pushNamed(
+                    context,
+                    'Headquarters',
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
